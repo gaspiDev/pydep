b54a492344e739db2eb50bc99f8b0bff50148ec6
gaspi <gaspi>
2025-02-19 00:41:11 -0300

    CRUD like operations working

--- main.py
+++ main.py
@@ -1,50 +1,85 @@
 import argparse
 import os
+import sys
 import toml
+import subprocess
 
 
-# Let's parse the arguments given in the command line.
+
+
+# start'ssparse the arguments given in action="store_true"ne.
 parser = argparse.ArgumentParser(description="pydep.")
-parser.add_argument("--path", type=str, default=".", help="Path of your Pipfile.")
-parser.add_argument("--list", action="store_true", help="List all packages.")
-parser.add_argument("--versionof", type=str, help="Version of the specified package.")
+parser.add_argument("--path", "-p", type=str, help="Path of your Pipfile.")
+parser.add_argument("--list", "-l", action="store_true", help="List all packages.")
+parser.add_argument("--versionof", "-vo", type=str, help="Version of the specified package.")
+parser.add_argument("--add","-a", type=str, help="Name of the package to install.")
+parser.add_argument("--update-all","-ua", action="store_true", help="Name of the package to update to the last version.")
+parser.add_argument("--remove","-r", type=str, help="Name of the package to uninstall.")
+
 args = parser.parse_args() # Stores the args given as booleans.
 
 # Check if a path of the pipfile was given otherwise try again.
 if not args.path:
-  print("You fool! Use the --path flag to navigate to your Pipfile directory.")
-  exit()
+  print("Must use the --path flag to navigate to your Pipfile directory.")
+  sys.exit()
 
+# Check of any option was given...
+elif not (args.list and args.versionof and args.add and args.update-all and args.remove):
+  print("At least one flag must be used besides --path/-p")
 # Store the directory of the app, so at the end we get back.
 original_dir = os.getcwd()
 
+# Changes the directory to the path given in the arguments.
+try:
+  os.chdir(args.path)
+except FileNotFoundError:
+  print(f"The path {args.path} doesn't exist.")
+  sys.exit()
+
 # Now the main logic.
 try:
-  os.chdir(args.path) # Changes the directory to the path given in the arguments.
-
-  if not os.path.exists("Pipfile"): # Search the Pipfile.
-    print("Pipfile not found.")
-    exit()
-
   with open("Pipfile", 'r') as file:
     config = toml.load(file)
     dependencies = config.get("packages", {}) # Read the dependencies.
-
+  
+  # List all packages.
   if args.list:
     print(dependencies)
 
-  if args.versionof:
-    version: str = dependencies[args.versionof]
+  # Print version of specified package
+  elif args.versionof:
+    version = dependencies[args.versionof]
     if version == "*":
       print(f"Version of {args.versionof} is not pinned.")
+    elif isinstance(version, dict):
+      print(version["version"][2:])
     else:
       print(f"{version[2:]}")
 
+  # Add a package to the dependencies.
+  elif args.add:
+    subprocess.run(args=["pipenv", "install", args.add], cwd=args.path)
+  
+  elif args.update_all:
+    subprocess.run(args=["pipenv", "update"], cwd=args.path, check=True)
+
+  # Remove a package from the dependencies.
+  elif args.remove:
+    subprocess.run(args=["pipenv", "uninstall", args.remove], cwd=args.path)
+  
 except toml.TomlDecodeError as e:
   print(f"Error decoding Pipfile: {e}")
+
+except FileNotFoundError as e:
+  print("""Pipfile not found.
+        \nIf you want to activate a Pipenv do the following:
+        \n1. Deactivate any active virtual enviorment.
+        \n2. Change directory with cd to the desired directory.
+        \n3. Run pipenv shell for creating the virtual enviorment.""")
 
 except Exception as e:
   print(f"An unexcepted error occurred: {e}")
 
 finally:
   os.chdir(original_dir)
+  sys.exit()
